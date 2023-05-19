6d3efb6a389889305a2e5e17f00d11f5482825cb
Imran Remtulla <imranr98@pm.me>
2023-05-19 19:20:20 -0400

    Progress (untested)

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-/**
-
-TODO:
-- Given a config with:
-  - A VC Container
-  - A folder
-  - An optional "notification" script (if none, use a default)
-  - An optional "prepare files" shell script (if none, use a default)
-- Watch the folder for new files and run the notification script when there are new files
-  - The default notification script generates a random string and sends a notification to ntfy.sh with that ID
-- Serve a simple webpage with a single password field
-- When someone uses the page:
-  - Use their password to open the VC Container
-  - Run the "prep files" script on the folder
-    - By default, this uses the files' "created date" to prefix file names with a datetime string
-  - Move all files in the folder into the container
-  - Close the container
-  - Return a result/error to show on the webpage
-    - Note: Given that it might take a long time to do this, ensure the connection is kept alive while they wait
-- Dockerize
-
-*/
-
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
@@ -31,7 +8,7 @@ const environment = require('./environment')
 fs.watch(environment.WATCH_DIR_PATH, (event, file) => {
   if (event == 'rename' && fs.existsSync(file)) {
     try {
-      console.log(child_process.execSync(`bash "${environment.NOTIF_SCRIPT_PATH}" ${environment.NOTIF_SCRIPT_ARGS || ''}`))
+      console.log(child_process.execSync(`bash "${environment.NOTIF_SCRIPT_PATH}" "${environment.WATCH_DIR_PATH}" ${environment.NOTIF_SCRIPT_ARGS || ''}`))
     } catch (e) {
       console.error(e)
     }
@@ -43,15 +20,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }))
 app.post('/process', (req, res) => {
+  let mountDir = null
   try {
-      if (!req.body.password) {
-          throw 'No password provided'
-      }
-      // TODO: Act
-      res.send()
+    if (!req.body.password) {
+      throw 'No password provided'
+    }
+    mountDir = fs.mkdtempSync()
+    child_process.execSync(`veracrypt -t --non-interactive -p '${req.body.password}' '${environment.VC_CONTAINER_PATH}' '${mountDir}'`)
+    child_process.execSync(`bash "${environment.PROCESS_SCRIPT_PATH}" "${mountDir}" ${environment.PROCESS_SCRIPT_ARGS || ''}`)
+    res.send()
   } catch (err) {
-      console.log(`Failed process attempt with body: ${JSON.stringify(req.body)}`)
-      console.log(err)
-      res.status(400).send()
+    console.log(`Failed process attempt with body: ${JSON.stringify(req.body)}`)
+    console.log(err)
+    res.status(400).send()
+  } finally {
+    try {
+      child_process.execSync(`veracrypt -t -d '${environment.VC_CONTAINER_PATH}'`)
+      if (mountDir) {
+        fs.unlinkSync(mountDir)
+      }
+    } catch {
+      // ignore
+    }
   }
 })
