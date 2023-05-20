9a77cbebfab7e3c42ed4439072568768d10ceeef
Imran Remtulla <imranr98@pm.me>
2023-05-19 23:57:25 -0400

    Bugs

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }))
 app.post('/handleDropoff', (req, res) => {
-  console.log('abc', req.body)
   let mountDir = null
   try {
     if (!req.body.password) {
